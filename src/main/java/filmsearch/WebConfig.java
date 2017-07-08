92452f44f1a2c7cb022d08af5ce52b89381d3f03
staspast <staspast1@gmail.com>
2017-07-08 16:12:45 +0200

    Added cors for front end

---
+++ src/main/java/filmsearch/WebConfig.java
@@ -0,0 +1,18 @@
+package filmsearch;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedOrigins("*");
+    }
+}
